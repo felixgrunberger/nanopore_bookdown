d9cfb33c1c03d421e7781cd81c5b7cde052f8e9a
felixgrunberger <felix-gruenebrger@web.de>
2017-12-20 16:05:17 +0100

    message

--- publish.R
+++ publish.R
@@ -1,4 +1,4 @@
 #compile book and publish to bookdown
 library(bookdown)
-render_book(input = "index.Rmd")
+render_book(input = "01-setup.Rmd")
 bookdown::publish_book(account = "felixgrunberger", server = "bookdown.org")
