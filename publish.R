356f34fd26b612d59c1be15761302350873008de
felixgrunberger <felix-gruenebrger@web.de>
2017-12-21 15:38:47 +0100

    new_stuff

--- publish.R
+++ publish.R
@@ -1,6 +1,9 @@
 #compile book and publish to bookdown
 library(bookdown)
 render_book(input = "index.Rmd")
-bookdown::publish_book(account = "felixgrunberger", server = "bookdown.org", name = "nanopore_bookdown")
+bookdown::publish_book(account = "felixgrunberger",
+                       server = "bookdown.org",
+                       name = "nanopore_bookdown")
+
 
 
